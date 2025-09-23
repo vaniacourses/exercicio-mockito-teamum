04269d6d7cc5ab52d7fef9bb78d9e1e118cdbd45
Bruno Oliveira <104176638+brxnx8@users.noreply.github.com>
2025-09-22 22:52:18 -0300

    Update JogoTest.java

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -17,7 +17,7 @@ public class JogoTest {
 	public void testarJogo(){
 		when(dado1.numero()).thenReturn(2);
 		when(dado2.numero()).thenReturn(5);
-		when(jogador.lancar(dado1, dado2)).thenReturn(dado1.numero(), dado2.numero());
+		when(jogador.lancar(dado1, dado2)).thenReturn(dado1.numero() + dado2.numero());
 
 		assertEquals(jogo.jogo(jogador, dado1, dado2), true)
 	}
