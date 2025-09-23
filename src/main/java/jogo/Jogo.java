4c4fe177a7c71c14e0a9142a26890f03daff603a
Bruno Oliveira <104176638+brxnx8@users.noreply.github.com>
2025-09-22 22:42:15 -0300

    consertando classe jogo, para poder ser testada (foi colocado parametros na função)

--- src/main/java/jogo/Jogo.java
+++ src/main/java/jogo/Jogo.java
@@ -4,18 +4,14 @@ public class Jogo {
 
 	
 	/*
-	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 você ganha o jogo;
-	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 você perde o jogo; 
-	 * Se não cair nenhum desses valores, o valor é armazenado e passa para o segundo turno;
-	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, você perde o jogo;
-	 * Se for o segundo turno, você continuando jogando os dados e só ganha se cair um numero igual ao anterior do primeiro turno;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 vocÃª ganha o jogo;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 vocÃª perde o jogo; 
+	 * Se nÃ£o cair nenhum desses valores, o valor Ã© armazenado e passa para o segundo turno;
+	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, vocÃª perde o jogo;
+	 * Se for o segundo turno, vocÃª continuando jogando os dados e sÃ³ ganha se cair um numero igual ao anterior do primeiro turno;
 	 */
-	public boolean jogo(){
-		
-		Jogador jogador = new Jogador();
-		Dado dadinho1 = new Dado();
-		Dado dadinho2 = new Dado();
-		
+	public boolean jogo(Jogador jogador, Dado dadinho1, Dado dadinho2){
+
 		int resultado = jogador.lancar(dadinho1, dadinho2);
 		
 		if(resultado == 7 || resultado == 11) {
